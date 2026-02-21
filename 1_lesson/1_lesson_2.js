4d544ec010ec61803b27f6508e1c962f3d81a4bf
studentsekiberone-prog <students.ekiberone@gmail.com>
2026-02-21 15:26:46 +0300

    Create 1_lesson_2.js

---
+++ 1_lesson/1_lesson_2.js
@@ -0,0 +1,41 @@
+// Условные операторы
+
+let time =14;
+
+if (time < 12) {
+  console.log("Доброе утро!");
+}else if (time < 18){
+  console.log("Добрый день!");
+}else {
+  console.log("Добрый вечер!");
+}
+
+// Циклы
+for (let i = 1; i <= 5; i++){
+  console.log(i);
+}
+
+let count = 10;
+
+while(count < 20){
+  console.log(count);
+  count++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
